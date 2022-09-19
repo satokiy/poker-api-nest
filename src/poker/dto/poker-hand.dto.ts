7ec9914b0b9dd8b35907e61011978d96d3be244b
satokiy <syamada@netprotections.co.jp>
2022-09-19 10:22:15 +0000

    OpenAPI定義書を充実させる Fixes #16

--- src/poker/dto/poker-hand.dto.ts
+++ src/poker/dto/poker-hand.dto.ts
@@ -3,6 +3,7 @@ import { IsNotEmpty, IsString } from 'class-validator';
 
 export class PokerHandDto {
   @ApiProperty({
+    type: String,
     description: '手札',
     example: 'H1 H2 H3 H4 H5',
     required: true,
@@ -14,20 +15,33 @@ export class PokerHandDto {
 
 export class PokerJudgeResponse {
   @ApiProperty({
+    type: String,
     description: '手札',
     example: 'H1 H2 H3 H4 H5',
   })
   hand: string;
 
   @ApiProperty({
+    type: [String],
     description: 'カードリスト',
     example: ['H1', 'H2', 'H3', 'H4', 'H5'],
   })
   cardList: string[];
 
   @ApiProperty({
+    enum: [
+      'STRAIGHT_FLASH',
+      'FLASH',
+      'STRAIGHT',
+      'ONE_PAIR',
+      'TWO_PAIR',
+      'THREE_CARD',
+      'FOUR_CARD',
+      'FULL_HOUSE',
+      'NO_ROLE',
+    ],
     description: '役',
-    example: "STRAIGHT_FLASH",
+    example: 'STRAIGHT_FLASH',
   })
   role: string;
 }
